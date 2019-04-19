8774d193390dc765be231b894133ecc39793e0e8
iugo <iugogogo@gmail.com>
2019-04-19 18:05:34 +0800

    调整 prettier 配置文件格式

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  trailingComma: "es5",
-  singleQuote: true,
-  endOfLine: "lf",
-  tabWidth: 2, // 默认值
-  printWidth: 80, // 默认值
-  bracketSpacing: true, // 默认值
-  semi: true // 默认值
-};
